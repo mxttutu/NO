0ca3d759c503713005605a3eb8522eb6b8e294ab
毛小兔maoxiaotu <90091247+mxttutu@users.noreply.github.com>
2022-04-06 15:40:18 +0800

    Add files via upload

---
+++ helpInGui/helpInGui/helpInGui.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "these are commmand line arguments, do not use Capitalisation\n";
+    std::cout << "type / error.make to make a custom js error";
+    std::cout << "type /runtime.collapse to end runtime\n";
+    std::cout << "type /connect.server to connect to the server\n";
+    std::cout << "type /load.server download the html value from the web\n";
+    std::cout << "type /clone.website to clone it\n";
+    std::cout << "type /build.localhost to launch the code\n";
+    std::cout << "type /gui to open a little GUI page type  /gui.ssl to open a little GUI page for ssl gen\n";}
+
+
+
+ 
